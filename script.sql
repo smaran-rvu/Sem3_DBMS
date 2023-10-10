0abd4ec7a644149c6b20915b5b26b26c85d5fa89
SmaranRB <smaranrbharadwaj@gmail.com>
2023-10-10 10:08:41 +0530

    First commit

---
+++ script.sql
@@ -0,0 +1,4 @@
+INSERT INTO BOOKS VALUES(195153448,  'Classical Mythology',  'Mark P. O. Morford',   2002,  'Oxford University Press')
+INSERT INTO BOOKS VALUES(1841721522,  'New Vegetarian: Bold and Beautiful Recipes for Every Occasion',  'Celia Brooks Brown',  2001,  'Ryland Peters and  Small Ltd')
+INSERT INTO BOOKS VALUES(375759778,  'Prague : A Novel',  'ARTHUR PHILLIPS',  2003,  'Random House Trade Paperbacks')
+INSERT INTO BOOKS VALUES(425163091,  'Chocolate Jesus',  'Stephan Jaramillo',  1998,  'Berkley Publishing Group')
